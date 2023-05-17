e9562b5799cd27a0e0e1669e6509c24a113040fb
William Huang <piscesweilun@winstar.com.tw>
2023-05-17 11:25:40 +0800

    Render all content to virtual layer first, the update the screen.

--- Entry-M_DHT/Entry-M_DHT.ino
+++ Entry-M_DHT/Entry-M_DHT.ino
@@ -1,3 +1,25 @@
+/******************************************************************************
+* Entry-M_DHT
+*   Temperature-Humidity Monitor for Arduino/Entry-M/M-Series
+*
+* Note:
+*   1. There are three framebuffer(or layer) in Entry-M / M-Series 
+*     a. Foreground: What you see on screen.
+*     b. Virtual layer: Backbuffer, which is the default rendering target.
+*     c. Background: Background image, which is used to 'clear' image.
+*     
+*     For performance and flicker issues, we should render all content to 
+*     virtual layer, and the update to foreground.
+*
+*   2. We can try commands using CleverSystem.exe, then copy the commands to our
+*     program, and modify it if necessary.
+*
+*   3. Once command was modified, the checksum should be recalculated before 
+*     sent to device.
+*
+* History:
+*   2023/05/16  Created.
+*******************************************************************************/
 #include "DHT.h"  // DHT sensor library by Adafruit
 
 #define BAUDRATE  19200
@@ -73,10 +95,22 @@ byte CheckSum(const char* data, int count) {
   return BCC;
 }
 
+/*
+*   Copy virtual layer(backbuffer) to foreground layer (frontbuffer)
+*/
+void UpdateScreen() {
+  static unsigned char XXX[20] = {0x01,0x14,0x02,0x04,0x31,0x03,0x00,0x00,0x00,0x00,0x00,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0x0A,0x53,0x0D};
+  WriteCommand(XXX, sizeof(XXX));
+  WaitResponse(100, 500);
+}
+
+/*
+*   Update temperature and display on screen
+*/
 void ShowTemperature(float deg) {
   ClearTemperature();   // Clear virtual layer
 
-  static unsigned char XXX[33] = {0x01,0x21,0x02,0x04,0x31,0x13,0x00,0x00,0x8C,0x00,0x50,0xFF,0xFF,0xFF,0x00,0x00,0x00,'T','e','m','p',':',' ','0','0','.','0',' ','c',' ',0x0A,0xA0,0x0D};
+  static unsigned char XXX[33] = {0x01,0x21,0x02,0x04,0x31,0x13,0x00,0x00,0x87,0x00,0x50,0xFF,0xFF,0xFF,0x00,0x00,0x00,'T','e','m','p',':',' ','0','0','.','0',' ','c',' ',0x0A,0x9B,0x0D};
   XXX[26] = (int)(deg * 10) % 10 + '0';
   XXX[24] = (int)deg % 10 + '0';
   XXX[23] = ((int)deg / 10) % 10 + '0';
@@ -88,15 +122,57 @@ void ShowTemperature(float deg) {
   WaitResponse(100, 500);
 }
 
-void ClearTemperature() {
-  static unsigned char XXX[16] = {0x01,0x10,0x02,0x04,0x41,0x00,0x8C,0x00,0x50,0x01,0x72,0x00,0x78,0x0A,0x29,0x0D};
+/*
+*   Update temperature to virtual layer(backbuffer)
+*/
+void UpdateTemperature(float deg) {
+  ClearTemperature();   // Clear virtual layer
+
+  static unsigned char XXX[33] = {0x01,0x21,0x02,0x04,0x31,0x10,0x00,0x00,0x87,0x00,0x50,0xFF,0xFF,0xFF,0x00,0x00,0x00,'T','e','m','p',':',' ','0','0','.','0',' ','c',' ',0x0A,0x98,0x0D};
+  XXX[26] = (int)(deg * 10) % 10 + '0';
+  XXX[24] = (int)deg % 10 + '0';
+  XXX[23] = ((int)deg / 10) % 10 + '0';
+  if(XXX[23] == '0') {
+    XXX[23] = ' ';
+  }
+  XXX[31] = CheckSum(XXX, sizeof(XXX) - 2);
   WriteCommand(XXX, sizeof(XXX));
   WaitResponse(100, 500);
 }
 
+/*
+*   Clear temperature on virtual layer(backbuffer)
+*/
+void ClearTemperature() {
+  static unsigned char XXX[16] = {0x01,0x10,0x02,0x04,0x41,0x00,0x87,0x00,0x50,0x01,0xDB,0x01,0x10,0x0A,0x26,0x0D};
+  WriteCommand(XXX, sizeof(XXX));
+  WaitResponse(100, 500);
+}
+
+/*
+*   Update humidity and display on screen
+*/
 void ShowHumidity(float hum) {
   ClearHumidity();
-  static unsigned char XXX[30] = {0x01,0x1E,0x02,0x04,0x31,0x13,0x00,0x00,0x9F,0x00,0x8C,0xFF,0xFF,0xFF,0x00,0x00,0x00,'H','u','m',':',' ','0','0',' ','%',' ',0x0A,0xE4,0x0D};
+
+  static unsigned char XXX[30] = {0x01,0x1E,0x02,0x04,0x31,0x13,0x00,0x00,0x9A,0x00,0x8C,0xFF,0xFF,0xFF,0x00,0x00,0x00,'H','u','m',':',' ','0','0',' ','%',' ',0x0A,0xDF,0x0D};
+  XXX[23] = (int)hum % 10 + '0';
+  XXX[22] = ((int)hum / 10) % 10 + '0';
+  if(XXX[22] == '0') {
+    XXX[22] = ' ';
+  }
+  XXX[28] = CheckSum(XXX, sizeof(XXX) - 2);
+  WriteCommand(XXX, sizeof(XXX));
+  WaitResponse(100, 500);
+}
+
+/*
+*   Update humidity on virtual layer(backbuffer)
+*/
+void UpdateHumidity(float hum) {
+  ClearHumidity();
+
+  static unsigned char XXX[30] = {0x01,0x1E,0x02,0x04,0x31,0x10,0x00,0x00,0x9A,0x00,0x8C,0xFF,0xFF,0xFF,0x00,0x00,0x00,'H','u','m',':',' ','0','0',' ','%',' ',0x0A,0xDC,0x0D};
   XXX[23] = (int)hum % 10 + '0';
   XXX[22] = ((int)hum / 10) % 10 + '0';
   if(XXX[22] == '0') {
@@ -108,7 +184,7 @@ void ShowHumidity(float hum) {
 }
 
 void ClearHumidity() {
-  static unsigned char XXX[16] = {0x01,0x10,0x02,0x04,0x41,0x00,0x9F,0x00,0x8C,0x01,0x5E,0x00,0xB4,0x0A,0xA0,0x0D};
+  static unsigned char XXX[16] = {0x01,0x10,0x02,0x04,0x41,0x00,0x9A,0x00,0x8C,0x01,0xDB,0x00,0xB4,0x0A,0x18,0x0D};
   WriteCommand(XXX, sizeof(XXX));
   WaitResponse(100, 500);
 }
@@ -119,7 +195,7 @@ void setup() {
   while (!Serial) ;  // wait for serial port to connect. Needed for native USB port only
 
   delay(2000);
-  ClearResponse();
+  ClearResponse();  // Clear noise after booting
 
   dht.begin();
 }
@@ -161,10 +237,7 @@ void loop() {
   Serial.println(F("°F"));
   */
 
-  ShowTemperature(t);
-  ShowHumidity(h);
-
-  // Clear read buffer
-  while(Serial.available())
-    Serial.read();
+  UpdateTemperature(t);
+  UpdateHumidity(h);
+  UpdateScreen();
 }
